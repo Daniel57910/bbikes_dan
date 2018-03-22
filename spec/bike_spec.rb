8a6930e49fa0f7a66cde322e7db2d2a291952a4d
Daniel Tooke <daniel.tooke@gmail.com>
2018-03-22 16:29:17 +0000

    chapter 19 part done

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,6 @@
 require 'app'
 
-describe Bike do 
+describe Bike do
     it { is_expected.to respond_to :working }
     it "new bike is working" do
       expect(Bike.new.working).to eq true
@@ -11,9 +11,11 @@ describe Bike do
       expect(broken_bike.broken).to eq false
     end
     it "allows to report a broken bike" do
-      docking_station = DockingStation.new
+      # Outside class called
+      station = double("DockingStation")
+      # docking_station = DockingStation.new
       bike = Bike.new
-      docking_station.dock(bike, true)
+      station.dock(bike, true)
       expect(bike.working).to eq false
     end
-end 
+end
